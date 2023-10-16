fix repetition precision bug for pythia

generate_from_repetition.py
@@ -7,20 +7,31 @@ import torch
 import torch.nn as nn
 
 import matplotlib.pyplot as plt
-from generate_from_logits import load_lm_head
+from train_head_utils import load_lm_head
 from repetition import compute_entropy
+from transformers import (
+    AutoTokenizer,
+)
+from lit_llama import Tokenizer
+
 
 DEVICE="cuda"
 # DTYPE = torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float32
 DTYPE = torch.float32
+DEFAULT_MODEL_DIRS = {
+    "llama": "/n/holystore01/LABS/barak_lab/Everyone/checkpoints/checkpoints/lit-llama",
+    "pythia": "/n/holystore01/LABS/barak_lab/Everyone/pythia",
+}
 
 def main(
     *,
     repetition_dir: str, 
-    output_dir: str,
+    model_type: str,
+    model_size: str,
     small_checkpoint_path: str = None,
+    tokenizer_path: str = None, 
+    output_dir: str = None,
     seed: int = 42,
-    model_size: str = "7B"
 ) -> None:
     """
     Args:
@@ -36,20 +47,48 @@ def main(
     random.seed(seed)
 
     if not small_checkpoint_path:
-        small_checkpoint_path = Path(f"/n/holystore01/LABS/barak_lab/Everyone/checkpoints/checkpoints/lit-llama/{model_size}/lit-llama.pth")
-    assert small_checkpoint_path.is_file()
+        default_model_dir = DEFAULT_MODEL_DIRS[model_type]
+        if(model_type == "llama"):
+            small_checkpoint_path = Path(f"{default_model_dir}/{model_size}/lit-llama.pth")
+        elif(model_type == "pythia"):
+            small_checkpoint_path = Path(f"{default_model_dir}/pythia-{model_size}/")
+        else:
+            raise ValueError
 
-    lm_head = load_lm_head(small_checkpoint_path, DTYPE)
+    assert small_checkpoint_path.exists()
+
+    if not tokenizer_path:
+        if(model_type == "llama"):
+            tokenizer_path = Path('/n/holystore01/LABS/barak_lab/Everyone/checkpoints/checkpoints/lit-llama/tokenizer.model')
+        elif(model_type == "pythia"):
+            pass # Not necessary
+
+    # load lm head
+    lm_head = load_lm_head(
+        small_checkpoint_path, dtype=DTYPE, device=DEVICE, model_type=model_type, model_size=model_size
+    )
     print("small lm head loaded")
+
+    # load tokenizer
+    global tokenizer 
+    if model_type == "pythia":
+        tokenizer= AutoTokenizer.from_pretrained(
+            f"EleutherAI/pythia-{model_size}",
+        )
+    elif model_type == "llama": 
+        tokenizer = Tokenizer(tokenizer_path)
 
     repetition_shards = os.listdir(repetition_dir)
     repetition_shards = list(sorted(repetition_shards, key=lambda x: int(x.split('_')[-1].strip('.pt'))))
     print(repetition_shards)
 
+    global encoded_prompts, large_entropy, original_prediction
+
     new_embed_all = []
     original_embed_all = []
     large_entropy = []
     prompt_type = []
+    encoded_prompts = []
     for repetition_shard in repetition_shards:
         shard_path = os.path.join(repetition_dir, repetition_shard)
         data = torch.load(shard_path, map_location=DEVICE)
@@ -59,16 +98,23 @@ def main(
         n_samples = data["new_embed"].shape[0]
         large_entropy.append(torch.concatenate(data["large_entropy"][0: n_samples]).cpu())
         prompt_type.append((data["prompt_type"][0: n_samples]).bool().cpu())
+        encoded_prompts += data["encoded_prompt"]
 
     new_embed_all = torch.concatenate(new_embed_all)
     original_embed_all = torch.concatenate(original_embed_all)
     large_entropy = torch.concatenate(large_entropy)
     prompt_type = torch.concatenate(prompt_type)
+    # print("shape checks: ", new_embed_all.shape, original_embed_all.shape, large_entropy.shape, prompt_type.shape, len(encoded_prompts))
     print("shape checks: ", new_embed_all.shape, original_embed_all.shape, large_entropy.shape, prompt_type.shape)
 
-    original_logits = lm_head(original_embed_all).detach()
+    original_logits = []
+    new_logits = []
+    for i in range(len(original_embed_all)):
+        original_logits.append(lm_head(original_embed_all[i]).detach())
+    original_logits = torch.stack(original_logits)
+
     new_logits = lm_head(new_embed_all).detach()
-    print("logits shape: ", original_logits.shape, new_logits.shape)
+    print("logits shape: ", original_logits.shape)
 
     original_probs = torch.softmax(original_logits, dim=-1)
     new_probs = torch.softmax(new_logits, dim=-1)
@@ -76,18 +122,25 @@ def main(
     original_entropy = compute_entropy(original_logits.cpu())
     new_entropy = compute_entropy(new_logits.cpu())
 
-    global img_output_dir
-    img_output_dir = output_dir
+    original_prediction = torch.argmax(original_probs, dim=-1)
 
-    plot_entropy(original_entropy.numpy(), new_entropy.numpy(), large_entropy.numpy(), prompt_type.numpy())
+    global img_output_dir
+    if output_dir is not None:
+        img_output_dir = output_dir
+    else: 
+        img_output_dir = os.path.join(os.path.dirname(repetition_dir), "results")
+        Path(img_output_dir).mkdir(parents=True, exist_ok=True)
+
+
+    # plot_entropy(original_entropy.numpy(), new_entropy.numpy(), large_entropy.numpy(), prompt_type.numpy())
 
     mutual_information(original_probs, new_probs, prompt_type)
 
-    probs_norm(original_probs, new_probs, prompt_type)
+    # probs_norm(original_probs, new_probs, prompt_type)
 
-    kl_divergence(original_probs, new_probs, prompt_type)
+    # kl_divergence(original_probs, new_probs, prompt_type)
 
-    weighted_entropy(original_entropy, new_entropy, original_probs, prompt_type)
+    # weighted_entropy(original_entropy, new_entropy, original_probs, prompt_type)
 
     return
 
@@ -97,10 +150,11 @@ def weighted_entropy(original_entropy, new_entropy, original_probs, prompt_type)
     p_x = torch.div(p_x, p_x.sum(dim=1, keepdim=True)).cpu()
 
     weighted_entropy = (new_entropy * p_x).sum(dim=1)
+    print("weighted entropy:", weighted_entropy.shape, (weighted_entropy<1.47).sum())
 
     plt.figure()
-    plt.hist(weighted_entropy[prompt_type].cpu().numpy()/ original_entropy[prompt_type].cpu().numpy(), bins=30, histtype="step", label="low_e_high_a")
-    plt.hist(weighted_entropy[~prompt_type].cpu().numpy()/original_entropy[~prompt_type].cpu().numpy(), bins=30, histtype="step", label="high_e_low_a")
+    plt.hist(weighted_entropy[prompt_type].cpu().numpy(), bins=30, histtype="step", label="low_e_high_a")
+    plt.hist(weighted_entropy[~prompt_type].cpu().numpy(), bins=30, histtype="step", label="high_e_low_a")
     plt.xlabel("Weighted Entropy")
     plt.ylabel("Count")
     plt.legend()
@@ -175,16 +229,16 @@ def mutual_information(x_probs, y_probs, prompt_type):
 
     print(top_k_idx.shape, y_probs.shape)
 
-    # # if choose to restrict the support of Y to only top K tokens
-    # p_y_given_x = torch.empty(p_x.shape[0], p_x.shape[1], p_x.shape[1], device=p_x.device)
-    # for i in range(top_k_idx.shape[0]):
-    #     ii_idx = top_k_idx[i]
-    #     for j in range(top_k_idx.shape[1]):
-    #         p_y_given_x[i, j, :] = y_probs[i, j, ii_idx]
-    # p_y_given_x = torch.div(p_y_given_x, p_y_given_x.sum(dim=2, keepdim=True))
+    # if choose to restrict the support of Y to only top K tokens
+    p_y_given_x = torch.empty(p_x.shape[0], p_x.shape[1], p_x.shape[1], device=p_x.device)
+    for i in range(top_k_idx.shape[0]):
+        ii_idx = top_k_idx[i]
+        for j in range(top_k_idx.shape[1]):
+            p_y_given_x[i, j, :] = y_probs[i, j, ii_idx]
+    p_y_given_x = torch.div(p_y_given_x, p_y_given_x.sum(dim=2, keepdim=True))
     
     # if choose support of Y to be all tokens
-    p_y_given_x = y_probs
+    # p_y_given_x = y_probs
 
     p_xy = torch.mul(p_x.unsqueeze(-1), p_y_given_x)
     
@@ -301,8 +355,6 @@ def plot_entropy(original_entropy, new_entropy, large_entropy, prompt_type):
     plt.legend()
 
     plt.subplot(2, 2, 4)
-    plt.hist(original_entropy[prompt_type].flatten() - new_entropy[prompt_type].max(axis=1), bins=50, histtype="step", label="MAX(new low_e_high_a)")
-    plt.hist(original_entropy[~prompt_type].flatten() - new_entropy[~prompt_type].max(axis=1), bins=50, histtype="step", label="MAX(new high_e_low_a)")
     plt.hist(original_entropy[prompt_type].flatten(), bins=30, histtype="step", label="original low_e_high_a", alpha=0.5)
     plt.hist(original_entropy[~prompt_type].flatten(), bins=30, histtype="step", label="original high_e_low_a", alpha=0.5)
     plt.xlabel("Entropy")
@@ -320,7 +372,7 @@ def plot_entropy(original_entropy, new_entropy, large_entropy, prompt_type):
     plt.xlabel("Original Entropy")
     plt.ylabel("Count")
     plt.legend()
-    plt.savefig("results/original_entropy.png")
+    plt.savefig(f"{img_output_dir}/original_entropy.png")
 
     print("--- original entropy classifier ---")
     simple_classification(torch.from_numpy(original_entropy).to(DEVICE), torch.from_numpy(prompt_type).to(DEVICE))
@@ -349,7 +401,7 @@ def simple_classification(x_train, y_train):
     optimizer = torch.optim.SGD(model.parameters(), lr=0.2)
 
     all_loss = []
-    for i in range(1001):
+    for i in range(2001):
         output = model(x_train)
 
         loss = criterion(output.view(-1), y_train.float())
@@ -360,9 +412,42 @@ def simple_classification(x_train, y_train):
         optimizer.zero_grad()
 
         if i % 200 == 0: 
-            prediction = output.view(-1) > 0
+            prediction = output.detach().view(-1) > 0
             accuracy = torch.sum((prediction==y_train).bool()) / y_train.shape[0]
             print(f"\tEpoch {i}, Loss:{loss.item():.3f}, Acc:{accuracy.item():.2f}")
+
+    # output = output.view(-1).detach()
+    # # most wrong examples
+    # for type in [0, 1]:
+    #     wrong_idx = torch.argwhere((prediction!=y_train)&(y_train == type)).view(-1)
+    #     wrong_idx_topk = torch.topk(output[wrong_idx], k=20, largest=not bool(type)).indices
+    #     wrong_idx = wrong_idx[wrong_idx_topk]
+    #     for p in wrong_idx:
+    #         if type == 0:
+    #             print(f"\n************ example: true type=epistemic, predicted type=aleatoric ************\n")
+    #         elif type == 1:
+    #             print(f"\n************ example: true type=aleatoric, predicted type=epistemic ************\n")
+    #         decoded_prompt = tokenizer.decode(encoded_prompts[p].squeeze())
+    #         decoded_prediction = tokenizer.decode(original_prediction[p])
+    #         print(f"{decoded_prompt} [{decoded_prediction} (H={large_entropy[p]:.2f})]")
+
+    # # most correct  examples
+    # for type in [0, 1]:
+    #     wrong_idx = torch.argwhere((prediction==y_train)&(y_train == type)).view(-1)
+    #     wrong_idx_topk = torch.topk(output[wrong_idx], k=20, largest=bool(type)).indices
+    #     wrong_idx = wrong_idx[wrong_idx_topk]
+    #     for p in wrong_idx:
+    #         if type == 0:
+    #             print(f"\n************ example: true type=epistemic, predicted type=epistemic ************\n")
+    #         elif type == 1:
+    #             print(f"\n************ example: true type=aleatoric, predicted type=aleatoric ************\n")
+
+    #         decoded_prompt = tokenizer.decode(encoded_prompts[p].squeeze())
+    #         decoded_prediction = tokenizer.decode(original_prediction[p])
+    #         print(f"{decoded_prompt} [{decoded_prediction} (H={large_entropy[p]:.2f})]")
+
+
+    return
 
 
     

precompute_logits.py
@@ -14,9 +14,6 @@ from transformers import (
     AutoTokenizer,
 )
 
-from lit_llama import LLaMA, Tokenizer
-from lit_llama.model import pipeLLaMA, LLaMAConfig
-from lit_llama.utils import EmptyInitOnDevice, jsd
 from train_head_utils import (
     load_llama,
     load_pythia,
@@ -24,7 +21,7 @@ from train_head_utils import (
 )
 
 
-DTYPE = torch.bfloat16
+DTYPE = torch.float32
 DEVICE = torch.device('cuda:0')
 SUPPORTED_MODEL_TYPES = set([
     "llama",
@@ -90,7 +87,7 @@ def main(
         if(model_type == "llama"):
             checkpoint_path = f'/n/holystore01/LABS/barak_lab/Everyone/checkpoints/checkpoints/lit-llama/{model_size}/lit-llama.pth'
         elif(model_type == "pythia"):
-            checkpoint_path = f'/n/holystore01/LABS/barak_lab/Everyone/models/pythia/pythia-{model_size}/'
+            checkpoint_path = f'/n/holystore01/LABS/barak_lab/Everyone/pythia/pythia-{model_size}/'
         else:
             raise ValueError
     
@@ -144,6 +141,7 @@ def main(
     skip = False
     outputs = {}
     for i, (key, prompt) in enumerate(prompts):
+        if shard_count == 5: break
         # Write shard
         if(i != 0 and i % output_shard_size == 0):
             if(len(outputs)):

repetition.py
@@ -5,30 +5,59 @@ from jsonargparse import CLI
 import torch
 import torch.nn as nn
 
-from lit_llama import Tokenizer
-from lit_llama.model import pipeLLaMA, LLaMA
-from lit_llama.utils import EmptyInitOnDevice
-from train_head_utils import PrecomputedShardLoader
 import json
 import pickle
 from pathlib import Path
 from tqdm import tqdm
+from train_head_utils import (
+    load_llama,
+    load_pythia,
+    MAX_LEN,
+    load_lm_head,
+)
+from transformers import (
+    AutoTokenizer,
+)
+from lit_llama import Tokenizer
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 DEVICE= "cuda"
-# DTYPE = torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float32
+# DTYPE = torch.bfloat16 if torchs.cuda.is_bf16_supported() else torch.float32
 DTYPE = torch.float32
 
 def log(file, *content, **kwargs):
     if file is not None:
         with open(file, "a") as f: print(content, **kwargs, file=file)
-    else: 
-        print(*content, **kwargs)
+    # else: 
+    #     print(*content, **kwargs)
 
+
+def pythia_forward(model, embeddings=False, return_after_layer_n=-1):
+    def fw(input_ids):
+        outputs = model.gpt_neox(
+            input_ids=input_ids,
+            attention_mask=torch.ones_like(input_ids),
+            output_hidden_states=True,
+            return_dict=True,
+        )
+
+        hidden_states = outputs.hidden_states[return_after_layer_n]
+
+        if(embeddings):
+            return hidden_states
+
+        logits = model.embed_out(hidden_states)
+        return logits
+
+    return fw
 
 def main(
     repetition_filter: str,
     shard_count: str, 
     prompts_json_path: str,
+    model_type: str,
     model_size: str,
     experiment_name: str = None,
     k: int = 10,
@@ -52,47 +81,37 @@ def main(
         example_path_print:  
     """
 
-    if(checkpoint_path is None):
-        checkpoint_path = Path(f"/n/holystore01/LABS/barak_lab/Everyone/checkpoints/checkpoints/lit-llama/{model_size}/lit-llama.pth")
+    if not checkpoint_path:
+        if(model_type == "llama"):
+            checkpoint_path = Path(f'/n/holystore01/LABS/barak_lab/Everyone/checkpoints/checkpoints/lit-llama/{model_size}/lit-llama.pth')
+        elif(model_type == "pythia"):
+            checkpoint_path = Path(f'/n/holystore01/LABS/barak_lab/Everyone/pythia/pythia-{model_size}/')
+        else:
+            raise ValueError
+        print(checkpoint_path)
+    
+    if not tokenizer_path:
+        if(model_type == "llama"):
+            tokenizer_path = Path('/n/holystore01/LABS/barak_lab/Everyone/checkpoints/checkpoints/lit-llama/tokenizer.model')
+        elif(model_type == "pythia"):
+            pass # Not necessary
 
-    if(tokenizer_path is None):
-        tokenizer_path = Path("/n/holystore01/LABS/barak_lab/Everyone/checkpoints/checkpoints/lit-llama/tokenizer.model")
+    assert checkpoint_path.exists()
+    if model_type == "llama": assert tokenizer_path.is_file()
 
-    assert checkpoint_path.is_file()
-    assert tokenizer_path.is_file()
-
-
-    # Initialize the model
-    # with EmptyInitOnDevice(
-    #     device=DEVICE, dtype=DTYPE, quantization_mode=None,
-    # ):
-    #     print("Loading model... ", end='')
-    #     t0 = time.time()
-    #     model = pipeLLaMA.from_name(model_size)
-    #     partition_schedule = model.partition_schedule
-    #     checkpoint = torch.load(checkpoint_path)
-    #     for key in list(checkpoint.keys()):
-    #         if 'transformer.h' in key:
-    #             split = key.split('.')
-    #             split[2] = partition_schedule[int(split[2])]
-    #             checkpoint[".".join(split)] = checkpoint.pop(key)
-    #     model.load_state_dict(checkpoint, strict=True)
-    #     print(f"Time: {time.time() - t0:.02f} seconds.")
-
-    with EmptyInitOnDevice(
-        device=DEVICE, dtype=DTYPE, quantization_mode=None,
-    ):
-        print("Loading model ...", end='', file=sys.stderr)
-        t0 = time.time()
-        model = LLaMA.from_name(model_size)
-        checkpoint = torch.load(checkpoint_path)
-        model.load_state_dict(checkpoint)
-        print(f"Time: {time.time() - t0:.02f} seconds.", file=sys.stderr)
+    # Initialize the model and tokenizer
+    if(model_type == "llama"):
+        model, tokenizer = load_llama(
+            model_size, checkpoint_path, tokenizer_path, DTYPE, None
+        )
+    elif(model_type == "pythia"):
+        model, tokenizer = load_pythia(
+            model_size, checkpoint_path, DTYPE
+        )
+    else:
+        raise ValueError(f"Invalid model type: {model_type}")
 
     model.eval()
-
-    # Initialize the tokenizer
-    tokenizer = Tokenizer(tokenizer_path)
 
     # load prompt indicator 
     shard_count = int(shard_count)
@@ -115,27 +134,50 @@ def main(
     prompt_type = []
     promot_indicator_sum = 0
     large_entropy_all = []
+    small_entropy_all = []
     for i, (prompt_key, promot_indicator) in enumerate(prompt_loader.items()):
         if promot_indicator.sum()  == 0: continue
-        print(prompt_key, promot_indicator.sum())
-        encoded_prompt = tokenizer.encode(prompts[prompt_key], bos=True, eos=False, device=DEVICE)
+        # print(prompt_key, promot_indicator.sum())
+        if model_type == "llama":
+            encoded_prompt = tokenizer(prompts[prompt_key])
+        elif model_type == "pythia":
+            encoded_prompt = tokenizer(prompts[prompt_key])
+        
+        # encoded_prompt = encoded_prompt[:MAX_LEN]
         large_entropy_array = large_entropy_dict[prompt_key]
         small_entropy_array = small_entropy_dict[prompt_key]
-        assert promot_indicator.shape[-1] == encoded_prompt.shape[0]
-        assert promot_indicator.shape[-1] == large_entropy_array.shape[0] == small_entropy_array.shape[0]
+        try:
+            assert promot_indicator.shape[-1] == encoded_prompt.shape[0]
+            assert promot_indicator.shape[-1] == large_entropy_array.shape[0] == small_entropy_array.shape[0]
+        except:
+            print("Exception!! Shape Mismatch - ignoring... ", promot_indicator.shape, encoded_prompt.shape)
+            continue
         promot_indicator_sum += promot_indicator.sum()
 
         for eligible_index in torch.argwhere(promot_indicator):
-            if eligible_index < 500:
-                encoded_prompts.append(encoded_prompt[None, :eligible_index+1])
+            if eligible_index < MAX_LEN:
+                assert promot_indicator[eligible_index] == 1
+                encoded_prompts.append(encoded_prompt[None, :eligible_index+1]) # confirmed index is correct
+                
                 large_entropy = large_entropy_array[eligible_index].double()
                 large_entropy_all.append(large_entropy)
+                
+                small_entropy = small_entropy_array[eligible_index].double()
+                small_entropy_all.append(small_entropy)
+    
                 prompt_type.append(large_entropy>0.2)
                 # small_entropy = small_entropy_array[eligible_index].double()
     prompt_type = torch.LongTensor(prompt_type)
-    log(example_path, prompt_type)
 
     print(f"{len(prompt_type)} encoded prompts , w/ {prompt_type.sum()} low_e_high_a examples.", file=sys.stderr) 
+
+    # plt.figure()
+    # plt.hist(np.array(small_entropy_all).flatten()[prompt_type.detach().cpu().bool().numpy()], bins=30, histtype="step", label="Small Model Original Entropy")
+    # plt.hist(np.array(small_entropy_all).flatten()[~prompt_type.detach().cpu().bool().numpy()], bins=30, histtype="step", label="Large Model Original Entropy")
+    # plt.xlabel("Original Entropy")
+    # plt.ylabel("Count")
+    # plt.legend()
+    # plt.savefig(f"Original_entropy.png")
 
     if experiment_name is not None:
         save_dir = os.path.join(repetition_filter, experiment_name)
@@ -143,17 +185,30 @@ def main(
 
     else:
         save_dir = repetition_filter
+
+    if model_type == "pythia":
+        tokenizer= AutoTokenizer.from_pretrained(
+            f"EleutherAI/pythia-{model_size}",
+        )
+    elif model_type == "llama":
+        tokenizer = Tokenizer(tokenizer_path)
+
+    small_lm_head = load_lm_head(
+        checkpoint_path, dtype=DTYPE, device=DEVICE, model_type=model_type, model_size=model_size
+    )
     
-    # large model small entropy example "high_e_low_a"
+    # repetition experiment
     new_embed_all = []
     orig_embed_all = []
+    encoded_prompt_all = []
+    t0 = time.time()
     for i, encoded_prompt in enumerate(encoded_prompts):
-        if i != 0 and i % 10 == 0: 
+        if i != 0 and i % 100 == 0: 
             print(f"{i}, {time.time() - t0:.02f} seconds.", file=sys.stderr)
             t0 = time.time()
         sys.stdout.flush()
         torch.cuda.empty_cache() 
-        orig_embed, repetition_embeds = repetition_experiment(model, encoded_prompt, tokenizer, k,
+        orig_embed, repetition_embeds = repetition_experiment(model, model_type, small_lm_head, encoded_prompt, tokenizer, k,
                                                               sample_until_period=sample_until_period,
                                                               addl_token_limit=addl_token_limit,
                                                               example_path=example_path)
@@ -163,32 +218,44 @@ def main(
             log(example_path, "low_e_high_a example: ")
         orig_embed_all.append(orig_embed)
         new_embed_all.append(repetition_embeds)
+        encoded_prompt_all.append(encoded_prompt.squeeze())
 
     new_embed_all = torch.concatenate(new_embed_all)
     orig_embed = torch.concatenate(orig_embed_all)
     torch.save({"new_embed": new_embed_all, 
                 "original_embed": orig_embed_all, 
                 "large_entropy": large_entropy_all, 
-                "prompt_type": prompt_type}, 
+                "prompt_type": prompt_type,
+                "encoded_prompt": encoded_prompt_all}, 
                 f'{save_dir}/repetition_{shard_count}.pt')
     
     return
 
 
-def repetition_experiment(model, encoded_prompt, tokenizer, k, 
+def repetition_experiment(model, model_type, small_lm_head, encoded_prompt, tokenizer, k, 
                           sample_until_period=True,
                           addl_token_limit=100,
                           example_path=None, 
                           ):
     
     len_prompt = encoded_prompt.shape[-1]
-    log(example_path, f"\nPrompt: \n {tokenizer.decode(encoded_prompt)}")
+    log(example_path, f"\nPrompt: \n {tokenizer.decode(encoded_prompt[0])}")
     # Run the model
-    with torch.no_grad():
-        orig_embed = model._forward(encoded_prompt).detach()
-        generated = model.lm_head(orig_embed).detach()
+    if model_type == "llama":
+        with torch.no_grad():
+            orig_embed = model._forward(encoded_prompt).detach()
+            generated = model.lm_head(orig_embed).detach()
+    elif model_type == "pythia":
+        with torch.no_grad():
+            orig_embed = pythia_forward(model, embeddings=True)(encoded_prompt)
+            generated = small_lm_head(orig_embed)
+
+    entropy = compute_entropy(generated[0, -1, :])
+    if entropy < 2.0 or entropy > 3.0:
+        print("entropy: ", generated.shape, entropy)
     generated = torch.softmax(generated, dim=-1).detach().cpu()
-    orig_embed = orig_embed[0, -1, :].detach().cpu()
+
+    orig_embed = orig_embed[0, -1, :].detach().cpu()        
 
     # Top k tokens
     log(example_path, "\nTop K Token: \n")
@@ -201,8 +268,13 @@ def repetition_experiment(model, encoded_prompt, tokenizer, k,
 
     # A surprise tool that will help us later
     # (a lone period produces a different token ID for some idiotic reason)
-    period_id = tokenizer.encode("Period.", bos=False, eos=False, device=DEVICE)[-1].item()
-
+    if model_type == "llama":
+        period_id = tokenizer.encode("Period.", bos=False, eos=False, device=DEVICE)[-1].item()
+        eos_id = tokenizer.eos_id
+    elif model_type == "pythia":
+        period_id = tokenizer.encode("Period.")[-1]
+        eos_id = tokenizer.encode("<|endoftext|>")[0]
+    
     repetition_embeds = []
     for t in torch.unbind(top_k):
         prompt_with_candidate = torch.cat(
@@ -216,7 +288,14 @@ def repetition_experiment(model, encoded_prompt, tokenizer, k,
         if(sample_until_period):
             addl_tokens = 0
             while True:
-                repetition_logits = model.forward(prompt_with_candidate).detach().cpu()
+                if model_type == "llama":
+                    with torch.no_grad():
+                        repetition_logits = model.forward(prompt_with_candidate).detach().cpu()
+                elif model_type == "pythia":
+                    with torch.no_grad():
+                        repetition_logits = pythia_forward(model)(prompt_with_candidate).detach().cpu()
+
+                # repetition_logits = model.forward(prompt_with_candidate).detach().cpu()
                 best_token = torch.argmax(repetition_logits, dim=-1)[:, -1].to(DEVICE)
                 prompt_with_candidate = torch.cat(
                     [
@@ -226,7 +305,7 @@ def repetition_experiment(model, encoded_prompt, tokenizer, k,
                     dim=-1
                 )
 
-                if(best_token == period_id or best_token == tokenizer.eos_id):
+                if(best_token == period_id or best_token == eos_id):
                     break
 
                 addl_tokens += 1
@@ -235,32 +314,59 @@ def repetition_experiment(model, encoded_prompt, tokenizer, k,
 
         log(example_path, "[prompt]", tokenizer.decode(prompt_with_candidate[0, len_prompt:]), end="<EOS> [prompt]")
 
-        repetition_prompt = torch.cat(
-            [
-                prompt_with_candidate,
-                torch.tensor(tokenizer.eos_id, device=DEVICE)[None, None],
-                encoded_prompt,
-            ],
-            dim=-1
-        )
+        if model_type == 'pythia':
+            repetition_prompt = torch.cat(
+                    [
+                        torch.tensor(eos_id, device=DEVICE)[None, None],
+                        prompt_with_candidate,
+                        torch.tensor(period_id, device=DEVICE)[None, None],
+                        torch.tensor(eos_id, device=DEVICE)[None, None],
+                        encoded_prompt,
+                    ],
+                    dim=-1
+                )
+        elif model_type == 'llama':
+            repetition_prompt = torch.cat(
+                    [
+                        prompt_with_candidate,
+                        # torch.tensor(tokenizer.eos_id, device=DEVICE)[None, None],
+                        encoded_prompt,
+                    ],
+                    dim=-1
+                )
 
-        with torch.no_grad():
-            repetition_embed = model._forward(repetition_prompt)[:, -1, :].detach().cpu()
-            repetition_embeds.append(repetition_embed)
-            if True:
-                repetition_logits = model.lm_head(repetition_embed.to(DEVICE))[0, :].detach()
-                repetition_logits = torch.softmax(repetition_logits, dim=-1)
-                repetition_top_k = torch.topk(repetition_logits, k, dim=-1).indices
-                decoded = [tokenizer.decode(rt) for rt in torch.unbind(repetition_top_k)]
-                prob = [float(repetition_logits[rt]) for rt in repetition_top_k]
-                for d, p in zip(decoded, prob):
-                    log(example_path, f"{d}: {p:.3f}", end=" ")
-                log(example_path, "\n")    
+        if model_type == 'llama':
+            with torch.no_grad():
+                repetition_embed = model._forward(repetition_prompt)[:, -1, :].detach().cpu()
+                repetition_embeds.append(repetition_embed)
+                if True:
+                    repetition_logits = model.lm_head(repetition_embed.to(DEVICE))[0, :].detach()
+                    repetition_logits = torch.softmax(repetition_logits, dim=-1)
+                    repetition_top_k = torch.topk(repetition_logits, k, dim=-1).indices
+                    decoded = [tokenizer.decode(rt) for rt in torch.unbind(repetition_top_k)]
+                    prob = [float(repetition_logits[rt]) for rt in repetition_top_k]
+                    for d, p in zip(decoded, prob):
+                        log(example_path, f"{d}: {p:.3f}", end=" ")
+                    log(example_path, "\n")    
+        elif model_type == "pythia":
+            with torch.no_grad():
+                repetition_embed = pythia_forward(model, embeddings=True)(repetition_prompt)[:, -1, :].detach().cpu()
+                repetition_embeds.append(repetition_embed)
+                if True:
+                    repetition_logits = model.embed_out(repetition_embed.to(DEVICE))[0, :].detach()
+                    repetition_logits = torch.softmax(repetition_logits, dim=-1)
+                    repetition_top_k = torch.topk(repetition_logits, k, dim=-1).indices
+                    decoded = [tokenizer.decode(rt) for rt in torch.unbind(repetition_top_k)]
+                    prob = [float(repetition_logits[rt]) for rt in repetition_top_k]
+                    for d, p in zip(decoded, prob):
+                        log(example_path, f"{d}: {p:.5f}", end=" ")
+                    log(example_path, "\n")    
+
     return orig_embed, torch.concatenate(repetition_embeds)[None, :]
 
 
 def compute_entropy(logits):
-    logits_softmax = torch.softmax(logits, dim=-1)
+    logits_softmax = torch.nn.functional.softmax(logits, dim=-1)
     logs = torch.nn.functional.log_softmax(logits, dim=-1)
     entropy = torch.sum(-1 * logits_softmax * logs, dim=-1)
     return entropy